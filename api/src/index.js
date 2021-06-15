da7d77944c55e1917f71279148e1e71cda0d907d
gusbdev <gusbdev@gmail.com>
2021-06-15 20:56:02 -0300

    Consuming api

--- api/src/index.js
+++ api/src/index.js
@@ -1,23 +1,15 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const axios = require("axios");
 
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(cors());
 
-async function callApi() {
-  await axios
-    .get(
-      "https://api.covid19api.com/country/brazil?from=2021-01-01T00:00:00Z&to=2021-06-01T00:00:00Z"
-    )
-    .then((response) => {
-      console.log(response.data);
-    });
-}
-
-app.use("/", (req, res) => {
-  callApi();
+app.use("/", async (req, res) => {
+  const { data } = await axios(
+    "https://api.covid19api.com/country/brazil?from=2021-01-01T00:00:00Z&to=2021-06-01T00:00:00Z"
+  );
+  return res.json(data);
 });
 
 app.listen(8080, () => {
