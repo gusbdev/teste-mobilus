432c7f848a8ab5ca1faf352c8ce90e4685a930f1
gusbdev <gusbdev@gmail.com>
2021-06-16 01:17:53 -0300

    Endpoint changed

--- api/src/index.js
+++ api/src/index.js
@@ -5,11 +5,11 @@ const axios = require("axios");
 
 app.use(cors());
 
-app.use("/", async (req, res) => {
+app.use("/cases", async (req, res) => {
   const { data } = await axios(
     "https://api.covid19api.com/country/brazil?from=2021-01-01T00:00:00Z&to=2021-06-01T00:00:00Z"
   );
-  return res.json(data);
+  return res.send(data);
 });
 
 app.listen(8080, () => {
